bcca0a6dd26c274a5100b990fea6c4a643b1a3f0
Olga <olga609@mail.ru>
2022-06-24 12:02:59 +0300

    Add Task_1

---
+++ Task_1/1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+int N = Convert.ToInt32(Console.ReadLine());
+        int num = 2;
+        while (num <= N) {
+            int res = num % 2 ;
+            if (res != 1) {
+                Console.WriteLine("Четное " + num);
+                num++;
+            }
+            else{
+                num++;
+            }
+
+        }
+    
+
+
